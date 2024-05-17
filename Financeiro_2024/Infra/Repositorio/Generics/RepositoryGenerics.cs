b1288d70b6e9df0589f72155a909374cffb7450b
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-05-17 10:49:08 +0100

    Implementando o repositorio base

--- Financeiro_2024/Infra/Repositorio/Generics/RepositoryGenerics.cs
+++ Financeiro_2024/Infra/Repositorio/Generics/RepositoryGenerics.cs
@@ -1,15 +1,17 @@
 ﻿using Domain.Interfaces.Generics;
 using Infra.Configuracao;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infra.Repositorio.Generics
 {
-    public class RepositoryGenerics<T> : InterfaceGeneric<T> where T : class
+    public class RepositoryGenerics<T> : InterfaceGeneric<T>, IDisposable where T : class
     {
         private readonly DbContextOptions<ContextBase> _OptionsBuilder;
 
@@ -60,5 +62,35 @@ namespace Infra.Repositorio.Generics
                 await data.SaveChangesAsync();
             }
         }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        // Public implementation of Dispose pattern called by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern
+        protected virtual void Dispose(bool disponsing)
+        {
+            if (disposed)
+                return;
+
+            if (disponsing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
     }
 }
