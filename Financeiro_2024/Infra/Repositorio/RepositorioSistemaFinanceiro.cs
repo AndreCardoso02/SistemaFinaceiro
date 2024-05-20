79ca6043e2c742018e279caed42109fed9aafc58
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-05-20 08:50:18 +0100

    Repositorio SIstema Financeiro

---
+++ Financeiro_2024/Infra/Repositorio/RepositorioSistemaFinanceiro.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces.Generics;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Entities.Entidades;
+using Infra.Repositorio.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioSistemaFinanceiro : RepositoryGenerics<SistemaFinanceiro>, InterfaceSistemaFinanceiro
+    {
+    }
+}
