8f513a5d1a8b9df1e359c345b1c42e3a28821c42
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-05-28 10:25:34 +0100

    Criando os endpoints de categoria

--- Financeiro_2024/WebApi/Controllers/CategoriaController.cs
+++ Financeiro_2024/WebApi/Controllers/CategoriaController.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Interfaces.ICategoria;
 using Domain.Interfaces.InterfaceServicos;
+using Entities.Entidades;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,45 @@ namespace WebApi.Controllers
         public async Task<object> ListarCategoriasUsuario(string emailUsuario)
         {
             return await _InterfaceCategoria.ListarCategoriasUsuario(emailUsuario);
+        }
+
+        [HttpPost("/api/AdicionarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> AdicionarCategoria(Categoria categoria)
+        {
+            await _ICategoriaServico.AdicionarCategoria(categoria);
+            return Ok(categoria);
+        }
+
+        [HttpPut("/api/ActualizarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> ActualizarCategoria(Categoria categoria)
+        {
+            await _ICategoriaServico.ActualizarCategoria(categoria);
+            return Ok(categoria);
+        }
+
+        [HttpGet("/api/ObterCategoria")]
+        [Produces("application/json")]
+        public async Task<object> ObterCategoria(int id)
+        {
+            return await _InterfaceCategoria.GetEntityById(id);
+        }
+
+        [HttpDelete("/api/RemoverCategoria")]
+        [Produces("application/json")]
+        public async Task<object> RemoverCategoria(int id)
+        {
+            try
+            {
+                var categoria = await _InterfaceCategoria.GetEntityById(id);
+                await _InterfaceCategoria.Delete(categoria);
+                return Ok(true);
+            }
+            catch (Exception)
+            {
+                return Ok(false);
+            }
         }
     }
 }
