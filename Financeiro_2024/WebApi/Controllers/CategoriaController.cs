23c8eb90dc68e1ef3a9c7627c6be2d8b765eb24a
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-05-28 10:19:19 +0100

    Criar o método de listar categorias de usuário

--- Financeiro_2024/WebApi/Controllers/CategoriaController.cs
+++ Financeiro_2024/WebApi/Controllers/CategoriaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.InterfaceServicos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
@@ -7,5 +9,19 @@ namespace WebApi.Controllers
     [ApiController]
     public class CategoriaController : ControllerBase
     {
+        private readonly InterfaceCategoria _InterfaceCategoria;
+        private readonly ICategoriaServico _ICategoriaServico;
+        public CategoriaController(ICategoriaServico iCategoriaServico, InterfaceCategoria interfaceCategoria)
+        {
+            _ICategoriaServico = iCategoriaServico;
+            _InterfaceCategoria = interfaceCategoria;
+        }
+
+        [HttpGet("/api/ListarCategoriasUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListarCategoriasUsuario(string emailUsuario)
+        {
+            return await _InterfaceCategoria.ListarCategoriasUsuario(emailUsuario);
+        }
     }
 }
