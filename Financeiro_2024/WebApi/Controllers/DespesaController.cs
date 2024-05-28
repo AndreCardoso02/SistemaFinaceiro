22f05a11153d7879d371067ead78c41b52121ca7
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-05-28 10:29:02 +0100

    Criando os endpoints da despesa

--- Financeiro_2024/WebApi/Controllers/DespesaController.cs
+++ Financeiro_2024/WebApi/Controllers/DespesaController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServicos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class DespesaController : ControllerBase
     {
+        private readonly InterfaceDespesa _InterfaceDespesa;
+        private readonly IDespesaServico _IDespesaServico;
+
+        public DespesaController(InterfaceDespesa interfaceDespesa, IDespesaServico iDespesaServico)
+        {
+            _InterfaceDespesa = interfaceDespesa;
+            _IDespesaServico = iDespesaServico;
+        }
+
     }
 }
